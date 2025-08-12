d2e034e68599717fbf8408420163eae25de6560f
Kanoa Guillermo <kanoa.guillermo@gmail.com>
2025-08-11 21:48:52 -0700

    finishing html in index.js

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,7 @@
 const functions = require("firebase-functions");
 const admin = require("firebase-admin");
 const nodemailer = require("nodemailer");
+const { ExportBundleInfo } = require("firebase-functions/v1/analytics");
 
 admin.initializeApp();
 
@@ -9,7 +10,27 @@ admin.initializeApp();
 const transporter = nodemailer.createTransport({
   service: "gmail", 
   auth: {
-    user: "tattedbycam@gmail.com",
-    pass:""
+    user: "tattedbycamie@gmail.com",
+    pass:"pjim fwca hopi xuzv"
   }
+});
+
+exports.sendEmailOnNewForm = functions.firestore.document('clients/{docId}').onCreate(async (snap,context) => {
+  const data = snap.data(); 
+  const clientEmail = data.email; 
+  const mailOptions = {
+    from: clientEmail,
+    replyTo: clientEmail, 
+    to: "tattedbycamie@gmail.com",
+    subject: "New Tattoo Consent Submission",
+    html: `
+    
+    `
+  };
+  try {
+    await transporter.sendMail(mailOptions);
+    console.log("Email sent!"); 
+   } catch (error) {
+    console.log("Error:", error);
+   }
 });
