c66e16605566a3b322c41a02f2ba816c5f5a5364
kanoag2003 <167948815+kanoag2003@users.noreply.github.com>
2025-08-26 13:58:23 -0700

    Done

--- src/App.jsx
+++ src/App.jsx
@@ -244,6 +244,7 @@ export default function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <QRCodeSVG className="QR-code "value="http://tattoo-consent-form.vercel.app" size={100}></QRCodeSVG>        
         <h1 style={{ fontFamily: "Dancing Script", fontWeight: "bold" }}>
           Tatted By Cam🖊️
         </h1>
